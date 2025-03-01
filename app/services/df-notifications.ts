5c4c05a308b7aa9a0e31ad52bda940a55d596a07
Yuriy Kurant <yuriy.kurant@gmail.com>
2025-03-02 01:33:57 +0800

    fix: sets notifications w/o autoClear

--- app/services/df-notifications.ts
+++ app/services/df-notifications.ts
@@ -5,9 +5,10 @@ export default class DFNotificationsService extends Service {
   @service declare notifications: NotificationsService;
 
   notifyError(msg: string) {
+    // NOTE: acceptance test `repositories: failed response` fails with `autoClear=true`
     this.notifications.error(msg, {
-      autoClear: true,
-      clearDuration: 2000,
+      // autoClear: true,
+      // clearDuration: 2000,
     });
   }
 }
